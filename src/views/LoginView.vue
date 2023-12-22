d2740761216b90c121cea4cf92247d8c6714180b
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2023-12-22 10:04:29 -0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,7 +5,7 @@
       <input type="text" v-model="username" placeholder="Usuário">
       <input type="password" v-model="password" placeholder="Senha">
       <button type="submit">Login</button>
-      token: {{ token }}
+      token: {{ username }}
     </form>
   </div>
 </template>
